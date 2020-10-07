d9d12e37c4357593248db511d8d47e9ae6547ae2
NouranBakry <nouranaymanbakry@gmail.com>
2020-10-07 13:58:44 +0200

    added navigation bar

--- unwind/src/App.js
+++ unwind/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Form from "./components/Form";
 import MeditationForm from "./components/MeditatationForm";
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
+        <Nav></Nav>
         <Form></Form>
         <Switch>
           <Route path="/meditate" component={MeditationForm}></Route>
